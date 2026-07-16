122dfc7db4709001c337d633b18c2c67c33a8283
vineet463 <vineetdubey266@gmail.com>
2026-07-16 23:37:55 +0530

    Update constraints.sql

--- sql/sql/constraints.sql
+++ sql/sql/constraints.sql
@@ -1,1 +1,36 @@
+ALTER TABLE products
+ADD CONSTRAINT fk_products_supplier
+FOREIGN KEY (supplier_id)
+REFERENCES suppliers(supplier_id);
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_orderitems_order
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
+
+ALTER TABLE order_items
+ADD CONSTRAINT fk_orderitems_product
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
+
+ALTER TABLE payment
+ADD CONSTRAINT fk_payments_order
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
+
+ALTER TABLE shipments
+ADD CONSTRAINT fk_shipments_order
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
+
+ALTER TABLE reviews
+ADD CONSTRAINT fk_reviews_product
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
+
+ALTER TABLE reviews
+ADD CONSTRAINT fk_reviews_customer
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id);
+
 
